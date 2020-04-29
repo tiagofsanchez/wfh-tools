e13cd834b01fc6930d9e7acab0a1b2852b2e80bd
Tiago Sanchez <tiagofsanchez@gmail.com>
2020-04-29 16:10:11 +0800

    fix: the feature banner

--- src/components/featured.js
+++ src/components/featured.js
@@ -1,39 +1,51 @@
 import React from "react"
 
-import {
-  IconButton,
-  makeStyles,
-  Card,
-  Typography,
-} from "@material-ui/core"
+import { IconButton, Card, Typography } from "@material-ui/core"
 import CloseIcon from "@material-ui/icons/Close"
 
-const useStyles = makeStyles({
-  root: {
-    width: "100%",
-    marginBottom: '50px',
-    height: "60px",
-    backgroundColor: "#ffb74d",
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-  title: {
-    padding: "10px 0 10px 20px",
-    fontWeight: "900",
-    color: "rgba(102, 51, 153, 1)",
-  },
-  link: {
-    textDecoration: "none",
-    borderBottom: "2px solid white",
-  },
-})
+// const useStyles = makeStyles({
+//   root: {
+//     width: "100%",
+//     marginBottom: "50px",
+//     height: "60px",
+//     backgroundColor: "#ffb74d",
+//     display: "flex",
+//     justifyContent: "space-between",
+//     alignItems: "center",
+//   },
+//   title: {
+//     padding: "10px 0 10px 20px",
+//     fontWeight: "900",
+//     color: "rgba(102, 51, 153, 1)",
+//   },
+//   link: {
+//     textDecoration: "none",
+//     borderBottom: "2px solid white",
+//   },
+// })
 
-const Featured = ({closeFeature}) => {
-  const classes = useStyles()
+const Featured = ({ closeFeature }) => {
+  // const classes = useStyles()
   return (
-    <Card className={classes.root}>
-      <Typography variant="body1" className={classes.title}>
+    <Card
+      style={{
+        width: "100%",
+        marginBottom: "50px",
+        height: "60px",
+        backgroundColor: "#ffb74d",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
+      <Typography
+        variant="body1"
+        style={{
+          padding: "10px 0 10px 20px",
+          fontWeight: "900",
+          color: "rgba(102, 51, 153, 1)",
+        }}
+      >
         Featured in UDACITY! Check out the{" "}
         <a
           className={classes.link}
